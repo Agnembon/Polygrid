ebb8663367d628b4be881836b7a105f1bf5ec0e4
Zalaya <enol.corzal@gmail.com>
2025-11-13 14:33:26 +0100

    enhancement: change some things, add some useCallbacks to the hook and remove the keys from the props

--- packages/polytable-react-package/src/components/Cell.tsx
+++ packages/polytable-react-package/src/components/Cell.tsx
@@ -1,8 +1,6 @@
-import type { CellCoordinates, CellValue } from '@/core/types';
-import type { Key } from "react";
+import type { CellCoordinates, CellValue } from "@/core/types";
 
 interface CellProps {
-  key: Key;
   value: CellValue;
   coordinates: CellCoordinates;
   isSelected: boolean;
@@ -10,13 +8,12 @@ interface CellProps {
   onMouseEnter: (coordinates: CellCoordinates) => void;
 }
 
-export const Cell = ({ key, value, coordinates, isSelected, onMouseDown, onMouseEnter }: CellProps) => (
+export const Cell = ({ value, coordinates, isSelected, onMouseDown, onMouseEnter }: CellProps) => (
   <td
-    key={key}
     onMouseDown={() => onMouseDown(coordinates)}
     onMouseEnter={() => onMouseEnter(coordinates)}
     className={`min-w-30 px-3 py-2 cursor-pointer select-none ${
-      isSelected ? 'bg-blue-200/60' : 'hover:bg-slate-200'
+      isSelected ? "bg-blue-200/60" : "hover:bg-slate-200"
     }`}
   >
     {value}
